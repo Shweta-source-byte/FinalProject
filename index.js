809da66dd8f36f4fa419abb911378fec7dec606e
Shweta-source-byte <shweta.joshi3008@gmail.com>
2022-04-06 00:51:07 +0530

    C

--- index.js
+++ index.js
@@ -4,29 +4,39 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const mongoclient = require('mongodb').mongoclient;
+const url = 'mongodb://localhost:27017/node_chat';
 
-
-
+mongoclient.connect(url, function(err,  db){
+const messagesCollection = db.collection('messages');
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+    res.sendFile(__dirname + '/index.html');
   });
-});
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
+  
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
     });
   });
   io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+      socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+
+        messagesCollection.insertOne({text:message}, function (err, res){
+            console.log('inserted a codument into the messagessCollection');
+        });
+      });
     });
-  });
+    io.on('connection', (socket) => {
+      socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+    });
+
+})
+
+
 
   
 server.listen(8080, () => {
