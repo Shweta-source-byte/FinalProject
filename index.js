c6f8dd08d469f1241af39293ed775b25f26faffb
Shweta-source-byte <shweta.joshi3008@gmail.com>
2022-04-07 01:13:01 +0530

    C

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 //MongoClient.connect(url, function(err,  db){
 MongoClient.connect(url, (err, db) => {
         //const messagesCollection = db.collection('chapApp');
-       const messagesCollection = db.messagesCollection;
+       const messagesCollection = db.messagesCollection ('chapApp');
        //const messagesCollection = db.collection ('chapApp');
 
 
